ac2f1c65aedbcdecccb104b7390ddeca6c35ef8b
Laniskefu <hansanyue@hotmail.com>
2021-10-02 15:56:20 +0800

    use environment variable to set dev mode

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 import argparse
+import os
 from game import Game
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('-d', type=str, default="", help='set developer mode')
+    parser.add_argument('-d', type=str, default=os.environ.get("DEV_MODE"), help='set developer mode')
     args = parser.parse_args()
 
     game = Game(developer_mode=bool(args.d))
