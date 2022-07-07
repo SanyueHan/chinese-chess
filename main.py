e8463b0abe9eb54a71c5faa4fc5dceb2632e1c69
SanyueHan <hansanyue@hotmail.com>
2022-07-08 00:31:22 +0800

    add mode echo

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+from config import DEVELOPER_MODE
 from game import Game
 
 
 if __name__ == "__main__":
+    if DEVELOPER_MODE:
+        print("DEVELOPER_MODE enabled")
     game = Game()
     game.play()
