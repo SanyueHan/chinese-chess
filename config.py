b9958ec47226de8cbe9ffa3946d23f51fa5a0fbe
SanyueHan <hansanyue@hotmail.com>
2022-07-11 00:41:46 +0800

    optimise code

--- config.py
+++ config.py
@@ -3,3 +3,7 @@ import os
 
 DEVELOPER_MODE = bool(os.environ.get("DEV_MODE"))
 AI_SEARCH_DEPTH = int(os.environ.get("AI_SEARCH_DEPTH", 4))
+
+
+print(f"DEVELOPER_MODE: {DEVELOPER_MODE}")
+print(f"AI_SEARCH_DEPTH: {AI_SEARCH_DEPTH}")
--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from config import DEVELOPER_MODE
 from game import Game
 
 
 if __name__ == "__main__":
-    if DEVELOPER_MODE:
-        print("DEVELOPER_MODE enabled")
     game = Game()
     game.play()
