d4d5ecdcc19a1d8787868dec4ab2700ec4a9e075
SanyueHan <hansanyue@hotmail.com>
2022-07-13 22:21:28 +0800

    update test script

--- test/test.py
+++ test/test.py
@@ -1,7 +1,8 @@
+from config import AI_SEARCH_DEPTH
 from engine.tree_searcher import TreeSearcher
 
 
-AI = TreeSearcher(4)
+AI = TreeSearcher(AI_SEARCH_DEPTH)
 
 
 with open("test/test.txt", "r") as f:
